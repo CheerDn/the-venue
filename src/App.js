6bfd313b31477fcea85c9c65796b1477e11bfb45
Cheer <cripaktcheer@gmail.com>
2020-04-18 21:58:44 +0800

    location, footer & scroll-to feature done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
 import React from 'react';
 import './resources/styles.css';
+import {Element} from 'react-scroll'
 
 import Header from './components/header_footer/Header';
 import Featured from './components/featured/';
 import VenueInfo from './components/venue_info/VenueInfo';
 import Highlights from './components/highlights/Highlights'
 import Pricing from './components/pricing/Pricing'
+import Location from './components/location/Location'
+import Footer from './components/header_footer/Footer'
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Featured />
-      <VenueInfo />
-      <Highlights />
-      <Pricing />
+
+      <Element name="featured">
+        <Featured />
+      </Element>
+
+      <Element name="venueInfo">
+        <VenueInfo />
+      </Element>
+
+      <Element name="highlights">
+        <Highlights />
+      </Element>
+
+      <Element name="pricing">
+        <Pricing />
+      </Element>
+
+      <Element name="location">
+        <Location />
+      </Element>
+      
+      <Footer />
     </div>
   );
 }
---
+++ src/components/location/Location.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Location = () => {
+  return (
+    <div className="location_wrapper">
+      <iframe 
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14457.99223592417!2d121.5498508!3d25.0511028!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x682251d5c6f37a58!2z5Y-w5YyX5bCP5beo6JuL!5e0!3m2!1szh-TW!2stw!4v1587210411363!5m2!1szh-TW!2stw" 
+        width="100%" 
+        height="520" 
+        frameBorder="0" 
+        allowFullScreen="" 
+      >
+      </iframe>
+      <div className="location_tag">
+        <div>Location</div>
+      </div>
+    </div>
+  );
+};
+
+export default Location;
