509e4977fa8a8fb3f1e3771149eb060e488d9317
Cheer <cripaktcheer@gmail.com>
2020-07-08 10:58:23 +0800

    apply prettier

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import './resources/styles.scss';
-import {Element} from 'react-scroll'
+import React from "react"
+import "./resources/styles.scss"
+import { Element } from "react-scroll"
 
-import Header from './components/header_footer/Header';
-import Featured from './components/featured/';
-import VenueInfo from './components/venue_info/VenueInfo';
-import Highlights from './components/highlights/'
-import Pricing from './components/pricing/Pricing'
-import Location from './components/location/Location'
-import Footer from './components/header_footer/Footer'
+import Header from "./components/header_footer/Header"
+import Featured from "./components/featured/"
+import VenueInfo from "./components/venue_info/VenueInfo"
+import Highlights from "./components/highlights/"
+import Pricing from "./components/pricing/Pricing"
+import Location from "./components/location/Location"
+import Footer from "./components/header_footer/Footer"
 
 function App() {
   return (
@@ -34,10 +34,10 @@ function App() {
       <Element name="location">
         <Location />
       </Element>
-      
+
       <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
